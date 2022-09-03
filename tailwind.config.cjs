200358d1e2117c53106017a6601e49db292ccdc0
Fractal-Tess <vgfractal@gmail.com>
2022-09-03 04:23:56 +0300

    removed white mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,18 +18,6 @@ const config = {
   daisyui: {
     themes: [
       {
-        light: {
-          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
-          primary: '#e863d6',
-          secondary: '#7bc3e0',
-          accent: '#cd97fc',
-          neutral: '#25343C',
-          'base-100': '#F8F8FB',
-          info: '#93BADC',
-          success: '#12A168',
-          warning: '#F4C857',
-          error: '#DE3B4E'
-        },
         dark: {
           ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
           primary: '#137cb5',
